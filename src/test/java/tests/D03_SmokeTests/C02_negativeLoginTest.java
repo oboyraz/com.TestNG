e5de729ffa7e903bf3a0b48e27f5053e8bc746e3
omerboyraz <omer.boyraz@isuzu.com.tr>
2023-01-11 10:24:39 +0300

    Smoke Test_negative Login Test

---
+++ src/test/java/tests/D03_SmokeTests/C02_negativeLoginTest.java
@@ -0,0 +1,46 @@
+package tests.D03_SmokeTests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import pages.QualitydemyPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C02_negativeLoginTest {
+    // wrong email, correct password
+    // correct email, wrong password
+    // wrong email, wrong password
+
+    QualitydemyPage qualitydemyPage = new QualitydemyPage();
+
+    @Test
+    public void wrongEmailTest(){
+        Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
+        qualitydemyPage.loginButton.click();
+        qualitydemyPage.emailArea.sendKeys(ConfigReader.getProperty("qdInvalidUsername"));
+        qualitydemyPage.passwordArea.sendKeys(ConfigReader.getProperty("qdValidPassword"));
+        qualitydemyPage.submitLoginButton.click();
+        Assert.assertTrue(qualitydemyPage.emailArea.isDisplayed());
+    }
+
+    @Test
+    public void wrongPasswordTest(){
+        Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
+        qualitydemyPage.loginButton.click();
+        qualitydemyPage.emailArea.sendKeys(ConfigReader.getProperty(("qdValidUsername")));
+        qualitydemyPage.passwordArea.sendKeys(ConfigReader.getProperty("qdInvalidPassword"));
+        qualitydemyPage.submitLoginButton.click();
+        Assert.assertTrue(qualitydemyPage.emailArea.isDisplayed());
+    }
+
+    @Test
+    public void wrongEmailWrongPasswordTest(){
+        Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
+        qualitydemyPage.loginButton.click();
+        qualitydemyPage.emailArea.sendKeys(ConfigReader.getProperty(("qdInvalidUsername")));
+        qualitydemyPage.passwordArea.sendKeys(ConfigReader.getProperty("qdInvalidPassword"));
+        qualitydemyPage.submitLoginButton.click();
+        Assert.assertTrue(qualitydemyPage.emailArea.isDisplayed());
+    }
+}
